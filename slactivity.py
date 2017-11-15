d99780de7b4f08190df7dac6cb6ee99d150e6618
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2017-11-14 22:45:51 -0600

    Print last message date for each channel

--- slactivity.py
+++ slactivity.py
@@ -1,13 +1,28 @@
+import time
 import argparse
 import requests
 
 
 SLACK_API_URL = "https://slack.com/api"
+RATE_LIMIT_SECONDS = 1
 
 
 class SlactivityGenerator:
     def __init__(self, token):
         self._token = token
+        self._last_slack_request = 0
+
+    def get_last_message(self, channel_id):
+        data = self._get_slack_data('conversations.history', 'channel={}'.format(channel_id))
+        messages = data['messages']
+
+        # find the first message which is an actual message (e.g. not a user leaving event)
+        for message in messages:
+            # we're only interested in a posted message, which will not have a subtype
+            if 'subtype' not in message:
+                return message
+
+        return None
 
     def get_channels(self):
         data = self._get_slack_data('channels.list', 'exclude_archived=true')
@@ -18,6 +33,11 @@ class SlactivityGenerator:
         for arg in api_args:
             url += '&' + arg
 
+        # wait for the rate limit seconds to pass between successive queries
+        while time.time() < self._last_slack_request + RATE_LIMIT_SECONDS:
+            time.sleep(.1)  # throttle the loop
+
+        self._last_slack_request = time.time()
         r = requests.get(url)
         r.raise_for_status()
 
@@ -30,7 +50,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     generator = SlactivityGenerator(args.token)
-    channels = generator.get_channels()
 
+    # print the date of the last message in each channel
+    channels = generator.get_channels()
     for channel in channels:
-        print(channel['name'])
+        msg = generator.get_last_message(channel['id'])
+
+        # if no message was ever posted, use the creation date of the channel
+        msg_ts = float(msg['ts']) if msg else float(channel['created'])
+        msg_date = time.strftime('%Y-%m-%d', time.gmtime(msg_ts))
+
+        print('{},{}'.format(channel['name'], msg_date))
